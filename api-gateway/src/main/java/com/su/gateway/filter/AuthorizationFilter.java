71dd4c839eb03c91156f319a15aa829be090c100
isushao <isushaopeng@gmail.com>
2021-01-11 17:40:21 +0800

    feat:网关添加全局过滤器

--- api-gateway/src/main/java/com/su/gateway/filter/AuthorizationFilter.java
+++ api-gateway/src/main/java/com/su/gateway/filter/AuthorizationFilter.java
@@ -1,12 +1,20 @@
 package com.su.gateway.filter;
 
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONObject;
+import cn.hutool.json.JSONUtil;
+import com.nimbusds.jose.JWSObject;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.logging.log4j.util.Strings;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
+
 
 /**
  * @ProjectName: my-spring-cloud
@@ -21,8 +29,28 @@ import reactor.core.publisher.Mono;
 @Slf4j
 public class AuthorizationFilter implements GlobalFilter, Ordered {
 
+    @SneakyThrows
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        // 统一鉴权逻辑
+        String token = exchange.getRequest().getHeaders().getFirst("Authorization");
+        if (StrUtil.isBlank(token)) {
+            return chain.filter(exchange);
+        }
+        token = token.replace("Bearer ", Strings.EMPTY);
+        JWSObject jwsObject = JWSObject.parse(token);
+        String payload = jwsObject.getPayload().toString();
+
+        // token(登出、修改密码)校验
+        JSONObject jsonObject = JSONUtil.parseObj(payload);
+        String jti = jsonObject.getStr("jti"); // JWT唯一标识
+        //todo something
+
+
+        ServerHttpRequest request = exchange.getRequest().mutate()
+                .header("payload", payload)
+                .build();
+        exchange = exchange.mutate().request(request).build();
 
         return chain.filter(exchange);
     }
