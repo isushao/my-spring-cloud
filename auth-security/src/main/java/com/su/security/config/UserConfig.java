7f233bd325069c297979aeb92b8ddfb2f07118ca
susp <isushao@163.com>
2021-02-26 09:17:13 +0800

    feat：修改security配置。。

--- auth-security/src/main/java/com/su/security/config/UserConfig.java
+++ auth-security/src/main/java/com/su/security/config/UserConfig.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -25,7 +26,7 @@ public class UserConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.requestMatchers()
-				.antMatchers("/login", "/oauth/**", "/actuator/**")
+				.antMatchers("/login", "/oauth/authorize", "/logout")
 				.and()
 				.authorizeRequests()
 				.anyRequest().authenticated()
